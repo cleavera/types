de3b9aac3daeb2b8beda4fd881b79dd748368e12
Anthony Cleaver <anthony.cleaver@mhr.co.uk>
2017-09-25 11:05:46 +0100

    Extend Number type for advanced mathematics

--- src/number/classes/number.ts
+++ src/number/classes/number.ts
@@ -23,6 +23,23 @@ export class $Number {
         return new this(numerator, denominator);
     }
 
+    public static parseInt(str: string, radix: number = 10): $Number {
+        const parts: [string, string] = str.split('.') as [string, string];
+
+        if (!parts[0]) {
+            throw new Error(`${str} is not a number`);
+        }
+
+        const numerator: number = parseInt(parts[0], radix);
+        const denominator: number = 1;
+
+        return new this(numerator, denominator);
+    }
+
+    public static identity(): $Number {
+        return this.fromString('1');
+    }
+
     public add(other: $Number): $Number {
         return new $Number((this.numerator * other.denomenator) + (other.numerator * this.denomenator), this.denomenator * other.denomenator);
     }
@@ -39,7 +56,51 @@ export class $Number {
         return new $Number(this.numerator * other.denomenator, this.denomenator * other.numerator);
     }
 
+    public power(power: number): $Number {
+        return new $Number(this.numerator ** power, this.denomenator ** power);
+    }
+
+    public modulus(mod: $Number): $Number {
+        const divisor: $Number = this.divide(mod);
+
+        return divisor.subtract(divisor.integer()).multiply(mod);
+    }
+
+    public negate(): $Number {
+        return this.multiply($Number.fromString('-1'));
+    }
+
+    public increment(): $Number {
+        return this.add($Number.identity());
+    }
+
+    public decrement(): $Number {
+        return this.subtract($Number.identity());
+    }
+
+    public factorial(): $Number {
+        let num: $Number = this.integer();
+        let result: $Number = this.integer();
+        const one: $Number =  $Number.identity();
+
+        while (num > one) {
+            num = num.decrement();
+
+            result = result.multiply(num);
+        }
+
+        return result;
+    }
+
+    public integer(): $Number {
+        return $Number.parseInt(this.toString());
+    }
+
     public toString(radix: number = 10): string {
         return (this.numerator / this.denomenator).toString(radix);
+    }
+
+    public valueOf(): number {
+        return Number(this.toString());
     }
 }
